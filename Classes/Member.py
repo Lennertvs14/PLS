80173185df72213c4e960dcaf1432344a8be5dd3
LennertvS <lennertvansplunder@gmail.com>
2023-03-02 16:09:55 +0100

    Member class changes
    This commit refactors the member class' fields to use consistent field names across the stored (JSON) objects
    and the Member class. This change was made to fix bugs and improve maintainability. In addition, a new validation has been added to ensure that usernames
     are unique.

--- Classes/Member.py
+++ Classes/Member.py
@@ -1,36 +1,37 @@
 from Classes.Person import Person
 import re
+import json
 
 
 class Member(Person):
     def __init__(self, given_name, surname, street_address, zip_code, city, email_address, username, password, telephone_number):
         super().__init__(username, password)
-        self.given_name = given_name
-        self.surname = surname
-        self.street_address = street_address
-        self.zip_code = zip_code
-        self.city = city
-        self.email_address = email_address
-        self.username = username
-        self.password = password
-        self.telephone_number = telephone_number
+        self.GivenName = given_name
+        self.Surname = surname
+        self.StreetAddress = street_address
+        self.ZipCode = zip_code
+        self.City = city
+        self.EmailAddress = email_address
+        self.Username = username
+        self.Password = password
+        self.TelephoneNumber = telephone_number
 
     @staticmethod
     def validate_field(field_name, input_value):
         input_value = input_value.strip()
-        special_validation_fields = ['email_address', 'zip_code', 'username', 'password']
+        special_validation_fields = ['EmailAddress', 'ZipCode', 'Username', 'Password']
         field_is_string = isinstance(field_name, str)
 
         if field_is_string:
             input_is_correct = isinstance(input_value, str) and 0 < len(input_value) < 100
             if field_name not in special_validation_fields:
                 return input_is_correct
-            elif input_is_correct and field_name == 'email_address':
+            elif input_is_correct and field_name == 'EmailAddress':
                 return Member.validate_email(input_value)
-            elif input_is_correct and field_name == 'zip_code':
+            elif input_is_correct and field_name == 'ZipCode':
                 return Member.validate_zip_code(input_value)
-            elif input_is_correct and field_name == 'username' or field_name == 'password':
-                return Member.validate_credentials(input_value)
+            elif input_is_correct and field_name == 'Username' or field_name == 'Password':
+                return Member.validate_credentials(field_name, input_value)
             else:
                 return False
         else:
@@ -49,8 +50,18 @@ class Member(Person):
         return re.match(zip_regex, zip_code) is not None
 
     @staticmethod
-    def validate_credentials(credential):
-        credential_lowercase = credential.lower()
-        credential_is_lowercase = credential == credential_lowercase
-        return credential_is_lowercase
+    def validate_credentials(field, credential):
+        validation = credential == credential.lower()
+        if validation and field == 'Username':
+            member_usernames = Member.__get_member_usernames()
+            if credential in member_usernames:
+                validation = False
+        return validation
 
+    @staticmethod
+    def __get_member_usernames():
+        file_path = "Data/Members.json"
+        with open(file_path) as file:
+            members = json.load(file)
+        usernames = [member["Username"] for member in members]
+        return usernames
