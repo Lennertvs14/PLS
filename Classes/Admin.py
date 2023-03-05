aeee888c2c93c261d300337a396144db06cfa470
LennertvS <lennertvansplunder@gmail.com>
2023-03-05 19:46:23 +0100

    Admin edit member functionality
    This method allows an admin to alter member credentials by user input.

--- Classes/Admin.py
+++ Classes/Admin.py
@@ -8,31 +8,6 @@ class Admin(Person):
         self.username = "admin"
         self.password = "admin123"
 
-    def delete_member(self):
-        members = self.get_members()
-        self.print_all_members(members)
-        try:
-            member_id = int(input("Give the identity of the user you would like to delete: "))
-            sorted_members = sorted(members, key = lambda m: int(m["Number"]))
-            member_to_delete = self.binary_search_for_member_by_identity(sorted_members, 0, len(sorted_members), member_id)
-            sorted_members.remove(member_to_delete)
-            self.update_members(sorted_members)
-        except:
-            print("Invalid member identity, please try again.")
-            self.delete_member()
-
-    def binary_search_for_member_by_identity(self, data, low, high, value):
-        if low > high:
-            return None
-        middle = (low+high)//2
-        member_id = int(data[middle]['Number'])
-        if member_id > value:
-            return self.binary_search_for_member_by_identity(data, low, middle - 1, value)
-        elif member_id < value:
-            return self.binary_search_for_member_by_identity(data, middle + 1, high, value)
-        else:
-            return data[middle]
-
     def add_member(self):
         members = self.get_members()
         new_member = self.create_member()
@@ -57,6 +32,12 @@ class Admin(Person):
                     print(f"Invalid {field_name} value, please try again.")
         return new_member
 
+    def delete_member(self):
+        sorted_members = sorted(self.get_members(), key = lambda m: int(m["Number"]))
+        member_to_delete = self.get_member_by_identity_input(sorted_members, True)
+        sorted_members.remove(member_to_delete)
+        self.update_members(sorted_members)
+
     def update_members(self, members):
         members_list_is_not_empty = len(members) > 0
         if members_list_is_not_empty:
@@ -78,3 +59,65 @@ class Admin(Person):
         with open(file_path) as file:
             members = json.load(file)
         return members
+
+    def edit_member(self):
+        member_to_edit = self.get_member_by_identity_input()
+        member_to_edit_identity = member_to_edit['Number']
+        member_to_edit.pop('Number')
+        for key in member_to_edit:
+            print(f"Would you like to edit the {key}?")
+            yes_or_no = input("Enter 1, 2 or 3 to choose:\n [1] Yes\n [2] No\n [3] Exit\n-> ").strip()
+            if yes_or_no == "1":
+                value = input(f"Please enter the {key}: ")
+                if value != "" and Member.validate_field(key, value):
+                    member_to_edit[key] = value
+                else:
+                    print("Invalid input.")
+            if yes_or_no == "3":
+                break
+        member_to_edit['Number'] = member_to_edit_identity
+        print(f"\n{member_to_edit}")
+        members = self.get_members()
+        for i, member in enumerate(members):
+            if member["Number"] == member_to_edit["Number"]:
+                members[i] = member_to_edit
+                self.update_members(members)
+                break
+
+    def get_member_by_identity_input(self, members = None, members_are_sorted = False):
+        """
+        Returns a member object based on their identity number.
+
+        :param members: A list of member objects to search.
+        :param members_are_sorted: A boolean flag indicating if the list is sorted.
+        :return: A member object if found, otherwise None.
+        """
+        if members is None:
+            sorted_members = sorted(self.get_members(), key=lambda m: int(m["Number"]))
+        elif not members_are_sorted:
+            sorted_members = sorted(members, key = lambda m: int(m["Number"]))
+        else:
+            sorted_members = members
+        self.print_all_members(sorted_members)
+        try:
+            member_id = int(input("Give the identity of the user you would like to move forward with: ").strip())
+            if 0 < member_id <= sorted_members[-1]['Number']:
+                member = self.binary_search_for_member_by_identity(sorted_members, 0, len(sorted_members), member_id)
+                return member
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid member identity, please try again.")
+            return self.get_member_by_identity_input(sorted_members, True)
+
+    def binary_search_for_member_by_identity(self, data, low, high, value):
+        if low > high:
+            return None
+        middle = (low+high)//2
+        member_id = int(data[middle]['Number'])
+        if member_id > value:
+            return self.binary_search_for_member_by_identity(data, low, middle - 1, value)
+        elif member_id < value:
+            return self.binary_search_for_member_by_identity(data, middle + 1, high, value)
+        else:
+            return data[middle]
