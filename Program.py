cf298def5cc513b6e2dcd1fa505497c0dd3d1fc5
LennertvS <lennertvansplunder@gmail.com>
2023-03-07 13:08:48 +0100

    Login functionality
    Admin and members are required to log in before using the system. They'll have 3 attempts to do so.

--- Program.py
+++ Program.py
@@ -1,5 +1,38 @@
+from Classes.Admin import Admin
+from Classes.Member import Member
+
 def main():
-    print("Hello, World!")
+    if isinstance(user, Admin):
+        print("Hi Admin!")
+    elif isinstance(user, Member):
+        print(f"Hi {user.GivenName}!")
+    else:
+        print("Something went wrong, please try again.")
+        return
+
+
+def login():
+    print("Welcome!\n")
+    members = library_admin.get_members()
+    attempts = 3
+    while attempts > 0:
+        username = input("Enter your username: ").strip()
+        password = input("Enter your password: ").strip()
+        # Admin validation
+        if username == "admin" and password == "admin123":
+            return library_admin
+        # Member validation
+        for i in range(len(members)):
+            if members[i]['Username'] == username and members[i]['Password'] == password:
+                return members[i]
+        attempts -= 1
+        print(f"You have {attempts} attempts left.")
+    return None
+
 
 if __name__ == "__main__":
-    main()
+    library_admin = Admin()
+    user = login()
+    user_logged_in = user is not None
+    if user_logged_in:
+        main()
